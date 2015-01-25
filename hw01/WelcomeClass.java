38f7f608f4cf3ad7ea63a6e0ebff048bb09036e7
ceo215 <ceo215@lehigh.edu>
2015-01-25 22:25:31 +0000

    completed WelcomeClass with my information

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,28 @@
+//Colin Orr
+//January 27, 2015
+//CSE 2
+
+//  first compile the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+// add main method
+    public static void main(String[] args) {
+        
+        
+// print the message
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-C--E--O--2--1--5->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("This past summer, I worked for Mars Chocolate and ate free candy every day.");
+        
+        //test/testtest
+    }
+}
